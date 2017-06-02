d08229c7dd42bf2ddce31a431d7b92a513caef79
Oliver Heyme <olihey@googlemail.com>
2017-06-02 09:50:31 +0200

    Added opendrive to rclone

--- fs/all/all.go
+++ fs/all/all.go
@@ -12,6 +12,7 @@ import (
 	_ "github.com/ncw/rclone/hubic"
 	_ "github.com/ncw/rclone/local"
 	_ "github.com/ncw/rclone/onedrive"
+	_ "github.com/ncw/rclone/opendrive"
 	_ "github.com/ncw/rclone/s3"
 	_ "github.com/ncw/rclone/sftp"
 	_ "github.com/ncw/rclone/swift"
